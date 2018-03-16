e491a04d93dd6dccb0452e6b14c7a1ce6bba9d04
Micah <mdemong1@comcast.net>
2018-03-15 22:29:32 -0700

    Added ClockHand implements MoveableShape, Stroke

---
+++ src/ClockHand.java
@@ -0,0 +1,25 @@
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.Stroke;
+
+public class ClockHand implements MoveableShape, Stroke {
+
+	@Override
+	public Shape createStrokedShape(Shape p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void draw(Graphics2D g2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void translate(int dx, int dy) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
