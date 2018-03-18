b6c4a602b2a1821d08dd8cc0ca12466c751b8365
Juan Macias <juan@jrmacias.com>
2018-03-18 16:34:29 -0700

    Modifying ClockHand.java for project to compile. Commented lines 23, 24, and 31.

--- src/ClockHand.java
+++ src/ClockHand.java
@@ -1,12 +1,10 @@
-import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.Stroke;
-import java.awt.Color;
+import java.awt.*;
+import java.awt.geom.Line2D;
 
 public class ClockHand implements MoveableShape, Stroke {
 	
 	//TODO: Add a timer
-/*
+
 	@Override
 	public Shape createStrokedShape(Shape p) {
 		// TODO Auto-generated method stub
@@ -22,15 +20,15 @@ public class ClockHand implements MoveableShape, Stroke {
 	@Override
 	public void translate(int dx, int dy) {
 		// TODO Auto-generated method stub
-		x += dx;
-        y += dy;
+		//x += dx;
+        //y += dy;
 
 	}
-	*/
+
 	
 	//I think this is how we draw, I pick 250 as the x1, x2, y2 
 	//because I the circe is 500 but I don't know if that is radius or diameter
-    @Override
+    //@Override
     public void paint(Graphics g) {
       Line2D shape = new Line2D.Double();
       shape.setLine(250, 20, 250, 250);  
@@ -38,6 +36,4 @@ public class ClockHand implements MoveableShape, Stroke {
 	  g2.setColor(Color.BLACK);
       g2.draw(shape);
    }
-}
-
 }
