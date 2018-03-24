acb909365b995541e15eff28dc6916e66cb747d4
Micah <mdemong1@comcast.net>
2018-03-23 20:30:08 -0700

    wrote MyClock constructor

--- src/MyClock.java
+++ src/MyClock.java
@@ -1,27 +1,33 @@
-import javax.swing.JLayeredPane;
+import javax.swing.*;
+import java.awt.*;
 
-public class MyClock extends JLayeredPane {
+public class MyClock extends JPanel {
 
-	//TODO: Should have 3 ClockHands, for hours, seconds minutes.
-	//TODO: Should have 1 ClockFace.
+	// TODO: Should have 3 ClockHands, for hours, seconds minutes.
+	// TODO: Should have 1 ClockFace.
+	// private ClockHand hours;
+	// private ClockHand mins;
+	// private ClockHand secs;
+	// private ClockFace face;
 
-	private JLayeredPane myClock;
 	private ClockFace clockface;
-	private ClockHand hand;
+	private ClockHand seconds;
 
-	  public MyClock(int x, int y, int width)
-   {
-	  myClock = new JLayeredPane();
-	  clockface = new ClockFace(x, y, width);
-	  hand = new ClockHand(1, 2, 3);
+	public MyClock(int x, int y, int width) {
+		clockface = new ClockFace(x, y, width);
+		seconds = new ClockHand(
+				(int)(width * SEC_RATIO), 
+				(x + width)/2, 
+				(y + width)/2, 
+				3.0f, 
+				Color.RED );
 
-
-	  myClock.add(clockface);
-	  myClock.add(hand);
-	  clockface.repaint();
-	  hand.repaint();
-
-
-   }
-
+		this.add(clockface);
+		this.add(seconds);
+		clockface.repaint();
+		seconds.repaint();
+		this.setOpaque(false);
+		this.setPreferredSize(new Dimension(width, width));
+	}
+	private static final double SEC_RATIO = 0.85;
 }
