1a510b99e743453b43f87d8e4a3fe703fb494ae5
Juan Macias <juan@jrmacias.com>
2018-03-24 15:33:34 -0700

    Stopwatch now properly shows tick values (increments of 5).

--- src/StopWatchDial.java
+++ src/StopWatchDial.java
@@ -1,33 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class StopWatchDial extends JLayeredPane{
-	private ClockFace face;
-	private ClockHand hand;
-	private int x;
-	private int y;
-	private int width;
-	
-	private static final double HAND_LENGTH = 0.8;
-	// length of hand relative to radius
-	
-	public StopWatchDial(int x, int y, int width) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.setLayout(new OverlayLayout(this));
-		face = new ClockFace(x, y, width);
-		int cX = x + width / 2;
-		int cY = y + width / 2;
-		hand = new ClockHand((int)((width / 2) * HAND_LENGTH), cX, cY, 2.0f, Color.BLACK);
- 		this.add(face, new Integer(1));
-		this.add(hand, new Integer(2));
-		this.setPreferredSize(new Dimension(width, width));
-		repaint();
-	}
+public class StopWatchDial extends JLayeredPane {
+    private ClockFace face;
+    private ClockHand hand;
+    private int x;
+    private int y;
+    private int width;
 
-	public void updateHand(int position){
-		double theta = ((double)position / 60) * 2 * Math.PI;
-		hand.setAngle(theta);
-	}
+
+    private static final double HAND_LENGTH = 0.8;
+    // length of hand relative to radius
+
+    public StopWatchDial(int x, int y, int width) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.setLayout(new OverlayLayout(this));
+        face = new ClockFace(x, y, width);
+        int cX = x + width / 2;
+        int cY = y + width / 2;
+        hand = new ClockHand((int) ((width / 2) * HAND_LENGTH), cX, cY, 2.0f, Color.BLACK);
+        this.add(face, new Integer(1));
+        this.add(hand, new Integer(2));
+        this.setPreferredSize(new Dimension(width, width));
+        face.setNumArr(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60);
+        repaint();
+
+    }
+
+    public void updateHand(int position) {
+        double theta = ((double) position / 60) * 2 * Math.PI;
+        hand.setAngle(theta);
+    }
 }
