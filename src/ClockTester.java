b1e520336e5423fefecd648c8a8a9f851a178980
Andrea Hernandez <andreaeu02@gmail.com>
2018-03-24 10:57:57 -0700

    Add listerners to ClockTester

--- src/ClockTester.java
+++ src/ClockTester.java
@@ -14,18 +14,41 @@ public class ClockTester
       
 
       MyClock clock = new MyClock(0, 0, CLOCK_RADIUS);
-
-      ClockFace face = new ClockFace(0, 0, CLOCK_RADIUS);
-
+//NO need to call ClockFace from here
+//      ClockFace face = new ClockFace(0, 0, CLOCK_RADIUS);
+	
+	  StopWatch stopwatch = new StopWatch(0, 0, CLOCK_RADIUS);
       
       frame.setLayout(new BorderLayout());
       frame.add(clock, BorderLayout.CENTER);
       
       JPanel topNav = new JPanel(new FlowLayout());
-      topNav.add(new JButton("clock"));
-      topNav.add(new JButton("stopwatch"));
+	  
+	  JButton clockButton = new JButton ("clock");
+	  
+	  clockButton.addActionListener(event -> {
+		frame.remove(stopwatch);
+		frame.add(clock, BorderLayout.CENTER);
+		clock.repaint();
+		frame.pack();
+	  });
+		
+		JButton stopwatchButton = new JButton ("stopwatch");
+		
+        stopwatchButton.addActionListener(event -> {
+		frame.getContentPane().remove(clock);
+		frame.pack();
+	//	frame.add(stopwatch, BorderLayout.CENTER);
+	//	clock.repaint();
+	  });
+
+
+	
+      topNav.add(clockButton);
+      topNav.add(stopwatchButton);
+	  
       frame.add(topNav, BorderLayout.NORTH);
-      clock.repaint();
+   //   clock.repaint();
 
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.pack();
