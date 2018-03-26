8a28d3ab9e856ab4e0bd37cebcfbdce6bc93c22f
Micah <mdemong1@comcast.net>
2018-03-25 21:28:06 -0700

    reorganized ClockTester

--- src/ClockTester.java
+++ src/ClockTester.java
@@ -6,20 +6,33 @@ import javax.swing.*;
 */
 public class ClockTester {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-
         MyClock clock = new MyClock(0, 0, CLOCK_RADIUS);
-
         StopWatch watch = new StopWatch(0, 0, CLOCK_RADIUS);
 
+        JFrame frame = new JFrame();
         frame.setLayout(new BorderLayout());
-        frame.add(clock, BorderLayout.CENTER);
 
         JPanel topNav = new JPanel(new FlowLayout());
-        JPanel lowNav = new JPanel();
-
+        frame.add(topNav, BorderLayout.NORTH);
+        
+        //On program start, clock is showing
+        frame.add(clock, BorderLayout.CENTER);
+        
         JButton clockButton = new JButton("Clock");
+        JButton stopwatchButton = new JButton("Stopwatch"); 
+        
+        topNav.add(clockButton);
+        topNav.add(stopwatchButton);
 
+        JButton stopButton = new JButton("Stop");
+        JButton startButton = new JButton("Start");
+        JButton resetButton = new JButton("Reset");
+        
+        JPanel lowNav = new JPanel();
+        lowNav.add(startButton);
+        lowNav.add(stopButton);
+        lowNav.add(resetButton);
+    
         clockButton.addActionListener(event -> {
             frame.remove(watch);
             frame.remove(lowNav);
@@ -27,41 +40,20 @@ public class ClockTester {
             frame.pack();
         });
 
-        JButton stopwatchButton = new JButton("Stopwatch");
-        JButton stopButton = new JButton("Stop");
-        JButton startButton = new JButton("Start");
-
-        JButton resetButton = new JButton("Reset");
-
-
-        lowNav.add(startButton);
-        lowNav.add(stopButton);
-        lowNav.add(resetButton);
-
         stopwatchButton.addActionListener(event -> {
             frame.remove(clock);
             frame.add(watch, BorderLayout.CENTER);
             frame.add(lowNav, BorderLayout.PAGE_END);
             frame.pack();
         });
-
+        
         stopButton.addActionListener(event -> watch.stop());
-
         startButton.addActionListener(event -> watch.start());
-
         resetButton.addActionListener(event -> watch.reset());
-
-
-        topNav.add(clockButton);
-        topNav.add(stopwatchButton);
-
-        frame.add(topNav, BorderLayout.NORTH);
-
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
-
     }
 
     private static final int CLOCK_RADIUS = 500;
